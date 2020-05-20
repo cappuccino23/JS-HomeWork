f97eff90e6e4d521266f7fd7a0b99059bc5b5d5d
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2020-05-20 18:01:40 +0300

    Update task_5.js
    solved task_5

--- HW_2/task_5.js
+++ HW_2/task_5.js
@@ -3,3 +3,62 @@
 переданного значения операции (использовать switch) выполнить одну из арифметических операций 
 (использовать функции из задания 4) и вернуть полученное значение. 
  */
+
+'use strict';
+
+    function dlt(a, b) {
+        let del;
+        if (b != 0)
+            del = a / b;
+        else
+            alert('Делить на ноль нельзя!');
+            del = undefined;
+            
+        return del; 
+    }
+
+    function sum(a, b) {
+        let summ;
+        summ = a + b;
+        return summ; 
+    }
+
+    function diff(a, b) {
+        let dff;
+        dff = a - b;
+        return dff; 
+    }
+
+
+    function mult(a, b) {
+        let mlt;
+        mlt = a * b;
+        return mlt; 
+    }
+
+
+    function choise_oper(a, b, oper) {
+        
+        switch(oper) {
+            case "*":
+                console.log("Результат умножения: " + mult(a, b));
+                break;
+            case "+":
+                console.log("Результат сложения:" + sum(a, b));
+                break;
+            case "-":
+                console.log("Результат вычитания:" + diff(a, b));
+                break;
+            case "/":
+                console.log("Результат деления"+ dlt(a, b));
+                break;
+            default:
+                console.log("некорректно введен знак");
+    }
+}
+ 
+let a = Number(prompt("Введите первое число: "));
+let b = Number(prompt("Введите второе число: "));
+let oper = prompt("Введите знак операции: ");
+
+result_operation = choise_oper(a, b, oper);
