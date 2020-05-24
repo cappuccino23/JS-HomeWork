3b60834bffc103645e4266dc27be94277fb306d2
alekskiseleva <51992975+alekskiseleva@users.noreply.github.com>
2020-05-24 13:31:06 +0300

    Update task_5.js
    solved task_5

--- HW_3/task_5.js
+++ HW_3/task_5.js
@@ -2,7 +2,11 @@
 Вам нужно: 
 1. Получить все товары, у которых есть фотографии, можете использовать метод filter https://mzl.la/2qROQkT 
 2. Отсортируйте товары по цене (от низкой цены к высокой), можете использовать метод sort https://mzl.la/2Y79hbZ, 
-как устроен sort можно посмотреть например здесь https://youtu.be/O2pusOp0gC0 */
+как устроен sort можно посмотреть например здесь https://youtu.be/O2pusOp0gC0 
+
+products[n].photos.length > 0
+        
+        */
  
 const products = [     
     {         
@@ -30,3 +34,27 @@ const products = [
         price: 78, 
     }, 
 ];
+
+// метод filter
+
+let products2 = products.filter(item => "photos" in item && item.photos.length > 0);
+console.log(products2);
+
+//метод for
+
+for (let n = 0; n <= products.length; n++) {
+    
+    if (products.find(item => item.photos !== -1))
+
+        if (products[n].photos !=0)
+            console.log(products[n]);
+
+    else
+        continue;
+}
+
+// сортируем массив обьектов с методом sort 
+products.sort(function(a,b) {
+     return a.price - b.price;
+ });
+ console.log(products);
